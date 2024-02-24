c84c2f1568411405f7f11af4c757b8b8b70e72e9
Meerahbel <mirabelugoji@gmail.com>
2024-02-23 21:01:09 -0800

    added prefabs

--- src/prefabs/HTML.jsx
+++ src/prefabs/HTML.jsx
@@ -38,14 +38,52 @@ export function h6$html() {
   return <h6>Heading 6</h6>;
 }
 
-export function p$html() {
-  return <p>Paragraph</p>;
-}
-
-export function a$html() {
-  return <a href="https://example.com">Link</a>;
-}
-
 export function img$html() {
   return <img src="https://picsum.photos/200/300" />;
+}
+
+export function Basic_Row() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: "20px",
+        justifyContent: "space-between",
+      }}
+    >
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+    </div>
+  );
+}
+
+export function Basic_Column() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        justifyContent: "space-between",
+      }}
+    >
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+      <div
+        style={{ width: "100px", height: "100px", backgroundColor: "#888" }}
+      />
+    </div>
+  );
 }
